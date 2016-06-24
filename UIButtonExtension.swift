e6b70412699bba9f3a46467d93095e46debaf707
Jx <jiangx.jason@gmail.com>
2016-06-24 10:42:30 +0800

    fix a bug
    Make it perfect

--- UIButtonExtension.swift
+++ UIButtonExtension.swift
@@ -12,18 +12,20 @@ extension UIButton {
     
     ///显示加载指示器
     func showActivityIndicatorStartAnimating(style: UIActivityIndicatorViewStyle?) {
-        let activityIndicator = UIActivityIndicatorView(frame: CGRect(origin: CGPointZero, size: CGSize(width: 20.0, height: 20.0)))
-        if style != nil {
-            activityIndicator.activityIndicatorViewStyle = style!
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) {
+            let activityIndicator = UIActivityIndicatorView(frame: CGRect(origin: CGPointZero, size: CGSize(width: 20.0, height: 20.0)))
+            if style != nil {
+                activityIndicator.activityIndicatorViewStyle = style!
+            }
+            activityIndicator.center = CGPoint(x: self.frame.width / 2.0, y: self.frame.height / 2.0)
+            dispatch_async(dispatch_get_main_queue()) {
+                self.titleLabel?.transform = CGAffineTransformMakeScale(0.015, 0.015)
+                self.imageView?.transform = CGAffineTransformMakeScale(0.015, 0.015)
+                self.addSubview(activityIndicator)
+            }
+            self.enabled = false
+            activityIndicator.startAnimating()
         }
-        activityIndicator.center = CGPoint(x: self.frame.width / 2.0, y: self.frame.height / 2.0)
-        dispatch_async(dispatch_get_main_queue()) {
-            self.titleLabel?.transform = CGAffineTransformMakeScale(0.0001, 0.0001)
-            self.imageView?.transform = CGAffineTransformMakeScale(0.0001, 0.0001)
-            self.addSubview(activityIndicator)
-        }
-        self.enabled = false
-        activityIndicator.startAnimating()
     }
     
     ///关闭加载指示器
